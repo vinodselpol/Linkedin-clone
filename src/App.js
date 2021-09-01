47c7b73b0eee7b115b68aaaf8d6340f2629f3e75
vinod selpol <vinodselpol@gmail.com>
2021-09-01 19:29:31 +0200

    ADD:sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import "./App.css";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <div className="app__body">
+        <Sidebar />
+        <Feed />
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar } from "@material-ui/core";
+
+function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p> {topic}</p>
+    </div>
+  );
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZR8MXx8fGVufDB8fHw%3D&w=1000&q=80"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2> Vinod Krishna</h2>
+        <h4> vinodselpol@gmail.com</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p> Who viewed you </p>
+          <p className="sidebar__statNumber"> 1,465</p>
+        </div>
+        <div className="sidebar__stat">
+          <p> Views on post </p>
+          <p className="sidebar__statNumber"> 2,896</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p> Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("developer")}
+        {recentItem("fontend")}
+        {recentItem("fullstack")}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
